d8125b30a1563bf43ec93800ecb3366c6e73c906
Jeremy Greer <jeremygreer@reergymerej.com>
2014-05-20 23:23:21 -0400

    add type identification

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 'use strict';
 
+var TYPES = {
+    String: 1,
+    Number: 1,
+    Boolean: 1,
+    Function: 1,
+    Array: 1,
+    Object: 1,
+    null: 1,
+    NaN: 1,
+    undefined: 1
+};
+
+var getType = function (x) {
+    var type;
+
+    if (x === null || x === undefined) {
+        type = x;
+    } else if (x.name) {
+        type = x.name;
+    } else {
+        type = (x).constructor.name;
+    }
+
+    return type;
+};
+
 var mod = function (foo) {
 
     var i,
-        values = {};
+        values = {},
+        type;
 
     if (!foo || foo.constructor.name !== 'Object') {
         throw new Error('an object is required');
@@ -12,16 +39,36 @@ var mod = function (foo) {
     // create private references
     for (i in foo) {
         if (foo.hasOwnProperty(i)) {
-            values[i] = foo[i];
+            type = getType(foo[i]);
+
+            values[i] = {
+                type: type,
+                initial: !(TYPES.hasOwnProperty(type)) ? foo[i] : undefined,
+                value: foo[i]
+            };
         }
     }
 
     return {
         get: function (prop) {
-            return values[prop];
+            return JSON.parse(JSON.stringify(values[prop].value));
         },
         set: function (prop, val) {
-            values[prop] = val;
+            var type = getType(val);
+
+            if (type === values[prop].type) {
+                values[prop].value = val;
+            } else {
+                throw new Error(val + ' should be a ' + type);
+            }
+        },
+        debug: function () {
+            for (var i in values) {
+                console.log(values[i]);
+            }
+        },
+        field: function (prop) {
+            return  values[prop];
         }
     };
 };
--- test/index.js
+++ test/index.js
@@ -23,7 +23,9 @@ describe('mod', function () {
             k: true,
             l: function () {},
             m: [1, 2, 3],
-            n: { foo: 'bar' }
+            n: { foo: 'bar' },
+            o: function SomeThing () {},
+            p: undefined
         };
     });
 
@@ -50,9 +52,138 @@ describe('mod', function () {
             expect(mod.get('i')).to.equal(foo.i);
         });
 
+        it('should not return values by reference', function () {
+            var m = mod.get('m').splice(1,1);
+            expect(mod.get('m')).to.include.members([1, 2, 3]);
+        });
+
         it('should provide setters for the properties', function () {
             mod.set('i', 'bar');
             expect(mod.get('i')).to.equal('bar');
+        });
+
+        describe('validation', function () {
+            it('should throw an error when setting the wrong type', function () {
+                expect(function () {
+                    mod.set('a', 1234);
+                }).to.throw(Error);
+            });
+        });
+
+        describe('field data', function () {
+            it('should be available through "field"', function () {
+                expect(mod.field('j')).to.eql({
+                    type: 'Number',
+                    initial: 123,
+                    value: 123
+                });
+            });
+
+            describe('types', function () {
+                describe('identifying from constructors', function () {
+                    it('should work for String', function () {
+                        expect(mod.field('a').type).to.equal('String');
+                    });
+
+                    it('should work for Number', function () {
+                        expect(mod.field('b').type).to.equal('Number');
+                    });
+
+                    it('should work for Boolean', function () {
+                        expect(mod.field('c').type).to.equal('Boolean');
+                    });
+
+                    it('should work for Function', function () {
+                        expect(mod.field('f').type).to.equal('Function');
+                    });
+
+                    it('should work for Array', function () {
+                        expect(mod.field('g').type).to.equal('Array');
+                    });
+
+                    it('should work for Object', function () {
+                        expect(mod.field('h').type).to.equal('Object');
+                    });
+
+                    it('should work for anonymous functions', function () {
+                        expect(mod.field('l').type).to.equal('Function');
+                    });
+
+                    it('should work for named functions', function () {
+                        expect(mod.field('o').type).to.equal('SomeThing');
+                    });
+                });
+
+                describe('identifying from literals', function () {
+                    it('should work for null', function () {
+                        expect(mod.field('d').type).to.equal(null);
+                    });
+
+                    it('should work for NaN', function () {
+                        expect(mod.field('e').type).to.equal('Number');
+                    });
+
+                    it('should work for undefined', function () {
+                        expect(mod.field('p').type).to.equal(undefined);
+                    });
+
+                    it('should work for strings', function () {
+                        expect(mod.field('i').type).to.equal('String');
+                    });
+
+                    it('should work for numbers', function () {
+                        expect(mod.field('j').type).to.equal('Number');
+                    });
+
+                    it('should work for booleans', function () {
+                        expect(mod.field('k').type).to.equal('Boolean');
+                    });
+
+                    it('should work for arrays', function () {
+                        expect(mod.field('m').type).to.equal('Array');
+                    });
+
+                    it('should work for objects', function () {
+                        expect(mod.field('n').type).to.equal('Object');
+                    });
+                });
+            });
+
+            describe('initial values', function () {
+                describe('when specifying only a type', function () {
+                    it('String should set no initial value', function () {
+                        expect(mod.field('a').type).to.equal(undefined);
+                    });
+
+                    it('Number should set no initial value', function () {
+                        expect(mod.field('b').type).to.equal(undefined);
+                    });
+
+                    it('Boolean should set no initial value', function () {
+                        expect(mod.field('c').type).to.equal(undefined);
+                    });
+
+                    it('Function should set no initial value', function () {
+                        expect(mod.field('f').type).to.equal(undefined);
+                    });
+
+                    it('Array should set no initial value', function () {
+                        expect(mod.field('g').type).to.equal(undefined);
+                    });
+
+                    it('Object should set no initial value', function () {
+                        expect(mod.field('h').type).to.equal(undefined);
+                    });
+
+                    it('anonymous functions should set no initial value', function () {
+                        expect(mod.field('l').type).to.equal(undefined);
+                    });
+
+                    it('named functions should set no initial value', function () {
+                        expect(mod.field('o').type).to.equal(undefined);
+                    });                 
+                });
+            });
         });
     });
 });
